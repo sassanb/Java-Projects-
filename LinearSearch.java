1484d11dc31bdfbe19219803ae51c81aae770c73
sassanb <Sassanbahramirad@yahoo.com>
2019-02-24 21:14:19 -0800

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,28 @@
+public class LinearSearch
+
+{
+	
+public static void main(String[] args)
+	
+{
+		
+int[] arr = {4, 6, 8, 7, 9, 2, 3, 1, 5, 0};
+		
+int target = 0;
+		
+		
+for (int i = 0; i < arr.length; i++) 
+{
+			
+if (arr[i] == target) 
+{
+				
+System.out.println(target + " found at position " + (i+1));
+			
+}
+		
+}
+	
+}
+
+}
